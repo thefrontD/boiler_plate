96511d521f6170a12c663a9e5854e9af5893d3a4
thefrontD_labtop <amigojmk@gmail.com>
2022-01-29 21:59:14 +0900

    editor not using form

--- client/src/components/views/EditorPage/EditorPage.js
+++ client/src/components/views/EditorPage/EditorPage.js
@@ -38,34 +38,66 @@ function LoginPage(props) {
   return (
     <div
       style={{
-        display: "flex",
         justifyContent: "center",
         alignItems: "center",
         width: "100%",
         height: "100vh",
       }}
     >
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={onSubmitHandler}
-      >
-        <label>작성자</label>
-        <label value={Name} />
-        <br />
-        <label>제목</label>
-        <input type="text" value={Title} onChange={onTitleHandler} />
-        <br />
-        <label>글 내용</label>
-        <br />
-        <textarea
-          type="text"
-          cols={100}
-          rows={10}
-          value={Content}
-          onChange={onContentHandler}
+      <div class="buttons">
+        <input
+          type="button"
+          class="BOLD"
+          value="B"
+          onclick="document.execCommand('bold')"
         />
-        <button>작성</button>
-      </form>
+        <input
+          type="button"
+          class="ITALIC"
+          value="Italic"
+          onclick="document.execCommand('Italic')"
+        />
+        <input
+          type="button"
+          class="UNDERBAR"
+          value="abc"
+          onclick="document.execCommand('Underline')"
+        />
+        <input
+          type="button"
+          class="BAR"
+          value="abc"
+          onclick="document.execCommand('StrikeThrough')"
+        />
+        <input
+          type="button"
+          class="aignLeft"
+          value="왼쪽 정렬"
+          onclick="document.execCommand('justifyleft')"
+        />
+        <input
+          type="button"
+          class="aignCenter"
+          value="가운데 정렬"
+          onclick="document.execCommand('justifycenter')"
+        />
+        <input
+          type="button"
+          class="aignRight"
+          value="오른쪽 정렬"
+          onclick="document.execCommand('justifyright')"
+        />
+      </div>
+      <br></br>
+      <p>글 내용</p>
+      <br />
+
+      <div>
+        <div class="editorDIV" contenteditable="true">
+          안녕하 d세요
+        </div>
+        <div class="editorHTMLDIV"></div>
+      </div>
     </div>
   );
 }
